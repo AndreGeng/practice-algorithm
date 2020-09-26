78215717ac5c177bb4eec45884b07ee15d50ae8c
山丘 <gengze@innotechx.com>
2020-09-26 09:58:42 +0800

    feat: leetcode6 zigzag add solution 2

--- leetcode-6-zigzag-conversion/index.ts
+++ leetcode-6-zigzag-conversion/index.ts
@@ -1,4 +1,4 @@
-export function convert(s: string, numRows: number): string {
+export function convert0(s: string, numRows: number): string {
   function getLineNum(idx: number) {
     if (numRows <= 1) return 1 
     if (numRows === 2) return (idx % 2) + 1
@@ -20,6 +20,28 @@ export function convert(s: string, numRows: number): string {
   }
   return result
 }
+export function convert(s: string, numRows: number): string {
+  let row = 1
+  let direction = 1 // 1: down, -1: up
+  const arr: string[] = []
+  for (let i=0;i<s.length;i++) {
+    arr[row] = (arr[row] || "") + s[i]
+    // 转向
+    if (direction < 0 && row === 1 || direction > 0 && row === numRows) {
+      direction = -direction
+    }
+    row += direction
+  }
+  let result = ""
+  for (let i =1;i<=numRows;i++) {
+    if (!arr[i]) {
+      break;
+    }
+    result += arr[i]
+  }
+  return result
+}
+
 
 // console.log(convert("PAYPALISHIRING", 3))
 // console.log(convert("", 1))
